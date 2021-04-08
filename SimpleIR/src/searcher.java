80a6a5b4b7267a0dff7997d9200b6e5705365768
yuran <hyur0920@gmail.com>
2021-04-08 10:01:33 +0900

    충돌 해결 후 master branch calcSim cosine 유사도로 변경

--- SimpleIR/src/searcher.java
+++ SimpleIR/src/searcher.java
@@ -59,21 +59,31 @@ public class searcher {
 		}
 	}	
 	private float calcSim(int id, KeywordList kl,HashMap hashmap) {
-	
+		float[] weight=innerProduct(id, kl, hashmap);
+		double qsize= Math.sqrt(kl.size()); //쿼리 형태소의 빈도수는 1로 고정이기 때문에 루트 형태소 개수와 동일
+		double dsize=0;
+		for(int i=1;i<weight.length;i++) {		
+			dsize+=Math.pow(weight[i], 2);
+		}
+		dsize= Math.sqrt(dsize);
+		float result=Math.round((weight[0]/(qsize*dsize))*100);
+		result=result/100;
+		return result;
 	}
-	private float innerProduct(int id, KeywordList kl,HashMap hashmap) {
-		float weight=0.0f;
+	private float[] innerProduct(int id, KeywordList kl,HashMap hashmap) {
+		float[] weight=new float[kl.size()+1];
 		for(int i=0;i<kl.size();i++) {
 			String keyword=kl.get(i).getString();
 			ArrayList<Float> al=(ArrayList<Float>)hashmap.get(keyword);
 			if(al.contains((float)id)) {
 				int index=al.indexOf((float)id);
 				float wit=al.get(index+1);
-				weight+=wit*1;//쿼리 형태소 가중치는 1로 고정
+				weight[0]+=wit*1;//쿼리 형태소 가중치는 1로 고정
+				weight[i+1]=wit; //0은 가중치 값 계산 1~kl.size()까지는 id 문서에 대한 각 단어의 가중치 저장
 			}	
 		}
-		weight=Math.round(weight*100);
-		weight=weight/100;
+		weight[0]=Math.round(weight[0]*100);
+		weight[0]=weight[0]/100;
 		return weight;
 	}
 	
@@ -98,6 +108,12 @@ public class searcher {
 					max=weight[i];
 					maxindex=i;
 				}	
+			}
+			if(max==0) {
+				if(k==3) { //제일 높은 유사도가 0이라는 의미
+					System.out.println("유사도가 있는 문서가 존재하지 않습니다.");
+				}
+				break;
 			}
 			Node doc=doclist.item(maxindex);
 			System.out.println(doc.getChildNodes().item(1).getTextContent());
